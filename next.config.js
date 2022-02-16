a90487a6b43763e3db08374b25c62ef90e27dc73
Anthony Mittz <anthonymittz@gmail.com>
2022-02-16 06:52:07 +0400

    Deploy to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProduction ? '/' : '',
-  basePath: '',
+  // basePath: '',
   reactStrictMode: true,
 }

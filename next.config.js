8cff8d5e32edc8507d430068beb214211b5bb113
RoscandDesign <59227392+Roscand@users.noreply.github.com>
2022-02-25 08:55:17 +0400

    Edit duplicates

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProduction ? '/' : '',
-  // basePath: '',
   reactStrictMode: true,
-  assetPrefix: '',
   basePath: ''
 }
--- parts/navigation/TopNavigation/TopNavigation.js
+++ parts/navigation/TopNavigation/TopNavigation.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import styles from './TopNavigation.module.css'
-import Link from 'next/link'
 
 export default function TopNavigation() {
   return <div className={styles.topNav}>

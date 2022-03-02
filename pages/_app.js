9e51347f36e37a7d6bd247b1eceff05dd0d3a516
RoscandDesign <59227392+Roscand@users.noreply.github.com>
2022-03-02 23:39:41 +0400

    Add page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/index.css'
 import Layout from '../parts/layout/Layout'
+import {SwitchTransition, CSSTransition} from 'react-transition-group'
 
-function MyApp({ Component, pageProps }) {
-  return <Layout><Component {...pageProps} /></Layout>
+function MyApp({ Component, pageProps, router }) {
+  return <Layout currentPath={router.pathname}>
+    <SwitchTransition mode="out-in">
+      <CSSTransition key={router.pathname} classNames="page" timeout={300} appear={true}>
+        <div className="page">
+          <Component {...pageProps} />
+        </div>
+      </CSSTransition>
+    </SwitchTransition>
+  </Layout>
 }
 
 export default MyApp
